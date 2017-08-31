71b454e8b07e08a563a411db72bfc0bf7ae8b866
Yabgu <30748723+Tomris-Yabgu@users.noreply.github.com>
2017-08-31 22:24:04 +0300

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,27 @@
+# Lambda ile ilgili örnekler
+#  The lambda fucktion's make many examples
+
+
+#Addition / toplama
+say =  lambda say1,say2:say1+say2
+print(say(3,4))
+
+#Multiplication/çarpma
+say =  lambda say1,say2:say1*say2
+print(say(3,4))
+
+#Division / bölme
+say =  lambda say1,say2:say1/say2
+print(say(12,4))
+
+#Subtraction /çıkarma
+say =  lambda say1,say2:say1-say2
+print(say(12,4))
+
+#Modulus/mod almak /moduler işlem
+say =  lambda say1,say2:say1%say2
+print(say(12,4))
+
+#pow / kuvvet,üst işlemleri
+say =  lambda say1,say2:say1**say2
+print(say(5,4))
